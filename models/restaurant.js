62c5f6195e72980a9fdd32b6f086f98d99649eb2
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-16 12:04:15 -0400

    check diffs for features not working

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,6 @@
 var Restaurant = can.Model.extend({
-  // these are staticProperties. there are some reserved properties: findAll, findOne, create, update, destroy 
-  findAll: 'GET /api/restaurants',
-  findOne: 'GET /api/restaurants/:id'
+	findAll: 'GET /api/restaurants',
+	findOne: 'GET /api/restaurants/:id'
 }, {
-  // including second, empty parameter object to set instanceProperties
+	// Include second, empty parameter object to set instanceProperties
 });
